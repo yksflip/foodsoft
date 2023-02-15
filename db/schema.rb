25d4efa71aa767255b9f24028b2b2ffcf0a3e80c
Philipp Rothmann <philipprothmann@posteo.de>
2023-02-15 09:59:02 +0100

    fix(messages): migrate message bodys to richtext

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_105256) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_085312) do
   create_table "action_text_rich_texts", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name", null: false
     t.text "body", size: :long
@@ -292,7 +292,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_105256) do
   create_table "messages", id: :integer, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.integer "sender_id"
     t.string "subject", null: false
-    t.text "body"
     t.boolean "private", default: false
     t.datetime "created_at", precision: nil
     t.integer "reply_to"
