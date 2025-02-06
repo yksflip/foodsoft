e4f62951fd8a4f0832c4a4e2c0d2ebb7c96551e3
Philipp Rothmann <philipprothmann@posteo.de>
2025-02-06 15:37:05 +0100

    Revert "set correct scope for all jobs in a generic way also for retries"
    This reverts commit e91fc18ef040f2bc8d6d0e27636108d5c03cf9f7.

--- config/initializers/active_job_select_foodcoop.rb
+++ config/initializers/active_job_select_foodcoop.rb
@@ -10,25 +10,9 @@ module FoodsoftActiveJobArguments
       end
 
       def deserialize(arguments)
-        FoodsoftConfig.select_multifoodcoop arguments[0]
+        FoodsoftConfig.select_multifoodcoop arguments.shift
         orig_deserialize(arguments)
       end
-    end
-  end
-end
-
-module ActiveJob
-  module Execution
-    # Store the original `_perform_job` method
-    original_perform_job = instance_method(:_perform_job)
-
-    # Override the `_perform_job` method
-    define_method(:_perform_job) do
-      foodsoft_scope = @arguments.shift
-      FoodsoftConfig.select_foodcoop foodsoft_scope
-
-      # Call the original `_perform_job` method
-      original_perform_job.bind(self).call
     end
   end
 end
